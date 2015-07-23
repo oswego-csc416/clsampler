65b792eb1d186d44c94528116896c3ba2b637ec9
Ting Qian <tqian86@gmail.com>
2015-07-23 10:56:39 -0400

    Removed the use of nrows in pandas when the input isn't a gzip

--- __init__.py
+++ __init__.py
@@ -97,7 +97,7 @@ class BaseSampler(object):
         if encoding == 'gzip':
             self.data = pd.read_csv(filepath, compression='gzip')
         else:
-            self.data = pd.read_csv(filepath, nrows=self.cutoff)
+            self.data = pd.read_csv(filepath)
 
         self.original_data = copy.deepcopy(self.data)
         if self.cutoff:
